60bcc500873f8d11965bc1ff63d5fc961a4e1fb8
tha zin soe <thazinsoe395@gmail.com>
2020-11-21 14:37:15 +0630

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h4>list</h4>
+    <h5>Unorder list</h5>
+      <ul>
+        <li>Apple</li>
+        <li>Orange</li>
+        <li>Banaa</li>
+      </ul>
+    <h5>order list</h5>
+    <ol>
+     
+      <li>Book</li>
+        <li>Pen</li>
+        <li>Pencil</li>
+      
+    </ol>
+</body>
+</html>
